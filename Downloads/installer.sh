49e440bc05a4cd85d176d4b5c4367d83dac03181
pvdhelm1974 <75042795+pvdhelm1974@users.noreply.github.com>
2022-01-16 11:08:06 +0100

    Add files via upload

--- Downloads/installer.sh
+++ Downloads/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#wget -q "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/installer.sh -O - | /bin/sh
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/installer.sh -O - | /bin/sh
 VERSION=1.3
 PLUGIN_PATH='/usr/lib/enigma2/python/Plugins/Extensions/MultiStalker'
 STALKER_CONF='/home/stalker.conf'
@@ -36,19 +36,19 @@ sleep 1;
 if grep -qs -i 'mips' cat $CHECK ; then
 	echo "[ Your device is MIPS ]"
     if [ $PYTHON = "PY3" ]; then
-	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/mipsel/multistalker$VERSION-py3-mipsel.tar.gz -O /tmp/multistalker$VERSION-py3-mipsel.tar.gz
+	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/mipsel/multistalker$VERSION-py3-mipsel.tar.gz -O /tmp/multistalker$VERSION-py3-mipsel.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py3-mipsel.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py3-mipsel.tar.gz
         
         if [ ! -f '/usr/lib/libpython3.7m.so.1.0' ]; then
-            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/mipsel/libpython3.7-mipsel.tar.gz -O /tmp/libpython3.7-mipsel.tar.gz
+            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/mipsel/libpython3.7-mipsel.tar.gz -O /tmp/libpython3.7-mipsel.tar.gz
             tar -xzf /tmp/libpython3.7-mipsel.tar.gz -C /
             rm -f /tmp/libpython3.7-mipsel.tar.gz
             
             echo "Send libpython3.7m"
         fi
     else
-        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py2/mipsel/multistalker$VERSION-py2-mipsel.tar.gz -O /tmp/multistalker$VERSION-py2-mipsel.tar.gz
+        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py2/mipsel/multistalker$VERSION-py2-mipsel.tar.gz -O /tmp/multistalker$VERSION-py2-mipsel.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py2-mipsel.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py2-mipsel.tar.gz
         
@@ -56,19 +56,19 @@ if grep -qs -i 'mips' cat $CHECK ; then
 elif grep -qs -i 'armv7l' cat $CHECK ; then
 	echo "[ Your device is armv7l ]"
     if [ $PYTHON = "PY3" ]; then
-	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/arm/multistalker$VERSION-py3-arm.tar.gz -O /tmp/multistalker$VERSION-py3-arm.tar.gz
+	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/arm/multistalker$VERSION-py3-arm.tar.gz -O /tmp/multistalker$VERSION-py3-arm.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py3-arm.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py3-arm.tar.gz
         
         if [ ! -f '/usr/lib/libpython3.7m.so.1.0' ]; then
-            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/arm/libpython3.7-arm.tar.gz -O /tmp/libpython3.7-arm.tar.gz
+            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/arm/libpython3.7-arm.tar.gz -O /tmp/libpython3.7-arm.tar.gz
             tar -xzf /tmp/libpython3.7-arm.tar.gz -C /
             rm -f /tmp/libpython3.7-arm.tar.gz
             
             echo "Send libpython3.7m"
         fi
     else
-        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py2/arm/multistalker$VERSION-py2-arm.tar.gz -O /tmp/multistalker$VERSION-py2-arm.tar.gz
+        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py2/arm/multistalker$VERSION-py2-arm.tar.gz -O /tmp/multistalker$VERSION-py2-arm.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py2-arm.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py2-arm.tar.gz
         
@@ -77,19 +77,19 @@ elif grep -qs -i 'armv7l' cat $CHECK ; then
 elif grep -qs -i 'aarch64' cat $CHECK ; then
 	echo "[ Your device is aarch64 ]"
     if [ $PYTHON = "PY3" ]; then
-	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/aarch64/multistalker$VERSION-py3-aarch64.tar.gz -O /tmp/multistalker$VERSION-py3-aarch64.tar.gz
+	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/aarch64/multistalker$VERSION-py3-aarch64.tar.gz -O /tmp/multistalker$VERSION-py3-aarch64.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py3-aarch64.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py3-aarch64.tar.gz
         
         if [ ! -f '/usr/lib/libpython3.7m.so.1.0' ]; then
-            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/aarch64/libpython3.7-aarch64.tar.gz -O /tmp/libpython3.7-aarch64.tar.gz
+            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/aarch64/libpython3.7-aarch64.tar.gz -O /tmp/libpython3.7-aarch64.tar.gz
             tar -xzf /tmp/libpython3.7-aarch64.tar.gz -C /
             rm -f /tmp/libpython3.7-aarch64.tar.gz
             
             echo "Send libpython3.7m"
         fi
     else
-        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py2/aarch64/multistalker$VERSION-py2-aarch64.tar.gz -O /tmp/multistalker$VERSION-py2-aarch64.tar.gz
+        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py2/aarch64/multistalker$VERSION-py2-aarch64.tar.gz -O /tmp/multistalker$VERSION-py2-aarch64.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py2-aarch64.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py2-aarch64.tar.gz
         
@@ -98,19 +98,19 @@ elif grep -qs -i 'aarch64' cat $CHECK ; then
 elif grep -qs -i 'sh4' cat $CHECK ; then
 	echo "[ Your device is sh4 ]"
 	if [ $PYTHON = "PY3" ]; then
-	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/sh4/multistalker$VERSION-py3-sh4.tar.gz -O /tmp/multistalker$VERSION-py3-sh4.tar.gz
+	    wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/sh4/multistalker$VERSION-py3-sh4.tar.gz -O /tmp/multistalker$VERSION-py3-sh4.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py3-sh4.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py3-sh4.tar.gz
         
         if [ ! -f '/usr/lib/libpython3.7m.so.1.0' ]; then
-            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py3/sh4/libpython3.7-sh4.tar.gz -O /tmp/libpython3.7-sh4.tar.gz
+            wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py3/sh4/libpython3.7-sh4.tar.gz -O /tmp/libpython3.7-sh4.tar.gz
             tar -xzf /tmp/libpython3.7-sh4.tar.gz -C /
             rm -f /tmp/libpython3.7-sh4.tar.gz
             
             echo "Send libpython3.7m"
         fi
     else
-        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/pvdhelm1974/IPTV-stalker/main/Downloads/py2/sh4/multistalker$VERSION-py2-sh4.tar.gz -O /tmp/multistalker$VERSION-py2-sh4.tar.gz
+        wget -q  "--no-check-certificate" https://raw.githubusercontent.com/ziko-ZR1/Multi-Stalker-install/main/Downloads/py2/sh4/multistalker$VERSION-py2-sh4.tar.gz -O /tmp/multistalker$VERSION-py2-sh4.tar.gz
         tar -xzf /tmp/multistalker$VERSION-py2-sh4.tar.gz -C /
         rm -f /tmp/multistalker$VERSION-py2-sh4.tar.gz
         
